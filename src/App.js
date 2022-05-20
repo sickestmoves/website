902914f3855110845d83f29fd3e4cc5d68ba5887
sickestmoves <81064490+sickestmoves@users.noreply.github.com>
2022-05-20 09:31:34 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Nischal is working on this site.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://instagram/justchalchal"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Instagram
         </a>
       </header>
     </div>
