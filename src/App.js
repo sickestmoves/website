807df761f4224951a652c82e9e7da7221c25a5b6
sickestmoves <81064490+sickestmoves@users.noreply.github.com>
2022-05-20 09:35:40 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://instagram/justchalchal"
+          href="https://instagram.com/justchalchal"
           target="_blank"
           rel="noopener noreferrer"
         >
